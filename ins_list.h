9335b7eb674f7b69b52786848793d604d276bd4d
Zdeněk Doležal <Griperis@outlook.cz>
2018-11-23 00:14:08 +0100

    uzins
    su

--- ins_list.h
+++ ins_list.h
@@ -96,7 +96,8 @@
 #define PI_LTE 63
 #define PI_GTE 64
 #define PI_NEQ 65
-
+#define PI_ADDSTR 66 //secte dva strinky
+#define PI_ADD 67	//secte dve cisla
 #define PI_ASS 80 //assign terminal int string or double.
 #define PI_ASSEND 81 //expression je hotovy, ulozit promennou
 #define PI_DEFPARAM 82
